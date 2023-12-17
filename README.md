21947a6d2ac424440d5e52bae028e6d1b548b34c
Michael Ottoson <michael.ottoson@cri.com>
2023-12-17 13:31:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# hypermea-core
+**The fastest way to build production-ready Hypermedia APIs.**
+
+
+
+![](https://github.com/pointw-dev/hypermea/blob/main/img/hypermea-letterhead.png?raw=True)
+
+
+
+> **Please note**:  although we currently use this toolkit to create production-ready APIs, the toolkit is still under development.  Use at your own risk.  This doc is under a heavy rewrite.  Information here is mostly correct, but there are gaps and it's messy.
+
+
+
+## Introduction
+
+This library is not intended to be used directly (though if you know what you are doing, you could).
+
+Instead, please see [hypermea](https://github.com/pointw-dev/hypermea) for the full toolkit and documentation.
---
+++ src/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from version import VERSION
+
+
+setup(
+    name='hypermea-core',
+    version=VERSION,
+    description='The library used by hypermea APIs.',
+    long_description=open('../README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Utilities'
+    ],
+    url='https://github.com/pointw-dev/hypermea',
+    author='Michael Ottoson',
+    author_email='michael@pointw.com',
+    packages=['hypermea'],
+    include_package_data=True,
+    zip_safe=False
+)
